4ea11e3070ce5fe75c00e2ac489bafd115f40e48
Nakul Joy <nakjoy@gmail.com>
2020-03-27 12:02:03 -0500

    Add files via upload

---
+++ biospace_news_current.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+response= requests.get("https://www.biospace.com/news/")
+soup = BeautifulSoup(response.text, 'html.parser')
+
+link= soup.select(".lister__header")
+#print(link)
+
+def list_articles(link):
+	articles=[]
+	for index, item in enumerate(link):
+		title= link[index].getText()
+		atab= link[index].a
+		href= atab.get('href') 
+		url= 'https://www.biospace.com'+href
+		articles.append({'title':title, 'link':url})
+	return articles
+
+pprint.pprint(list_articles(link))
+
+with open('Current_BioSpace_Articles.txt', mode='w') as file:
+	text_file=(list_articles(link)) 
+	file.write(f"{text_file}")
+
+
+
+
